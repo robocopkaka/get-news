a52f60eafb67eac8af3d49bc90cc0d47a37524d8
Okereke Onyekachi <robocopkaka@gmail.com>
2017-09-04 03:47:11 +0100

    Added some seed data for sources.

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,2 +1,3 @@
 class Source < ApplicationRecord
+  validate_uniqueness_of :name, :shortcode
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Source.create(name: "ABC News (AU)", shortcode: "abc-au", logo: "abc-au.png")
+Source.create(name: "Al Jazeera English", shortcode: "al-jazeera-english", logo: "al-jazeera-english.png")
+Source.create(name: "Ars Technica", shortcode: "ars-technica", logo: "ars-technica.png")
